1b1224739f83d7d20ceae900ffe5bddbef343776
kojima kentaro <70255049+kojimakentaro@users.noreply.github.com>
2021-02-04 22:16:42 +0900

    Add files via upload

---
+++ a.rb
@@ -0,0 +1,27 @@
+time = Time.new
+
+p time
+
+
+age=20
+puts "I am + #{age} + years old"
+
+
+#起動済
+
+#日本円をユーロやドルに変換
+
+jp=110
+
+us=jp*100
+uro=jp*130
+pn=jp*150
+
+puts us
+puts uro
+puts pn
+
+#起動済
+
+
+
